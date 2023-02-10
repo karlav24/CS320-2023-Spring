a0985f1f3a73d5af7890a22adad0087d9fe70aea
karlav24 <90121031+karlav24@users.noreply.github.com>
2023-02-10 18:16:42 -0500

    updated code

--- assigns/02/MySolution/assign02_03.py
+++ assigns/02/MySolution/assign02_03.py
@@ -14,36 +14,36 @@ def mylist_mergesort(xs):
     if mylist_nilq(xs) or mylist_nilq(xs.cons2):
         return xs
     else:
-        def split_list(lst, mid, left, right):
+        def split(lst, mid, left, right):
             if mylist_nilq(lst):
                 return (left, right)
             else:
                 if mid == 0:
                     right = mylist_cons(lst.cons1, right)
-                    return split_list(lst.cons2, mid, left, right)
+                    return split(lst.cons2, mid, left, right)
                 else:
                     left = mylist_cons(lst.cons1, left)
-                    return split_list(lst.cons2, mid-1, left, right)
+                    return split(lst.cons2, mid-1, left, right)
         
         mid = mylist_size(xs) // 2
         left = mylist_nil()
         right = mylist_nil()
         
-        left, right = split_list(xs, mid, left, right)
+        left, right = split(xs, mid, left, right)
         
         left = mylist_mergesort(left)
         right = mylist_mergesort(right)
         
-        return mylist_merge(left, right)
+        return merge(left, right)
 
-def mylist_merge(left, right):
+def merge(left, right):
     if mylist_nilq(left):
         return right
     elif mylist_nilq(right):
         return left
     else:
         if left.cons1 < right.cons1:
-            return mylist_cons(left.cons1, mylist_merge(left.cons2, right))
+            return mylist_cons(left.cons1, merge(left.cons2, right))
         else:
-            return mylist_cons(right.cons1, mylist_merge(left, right.cons2))
+            return mylist_cons(right.cons1, merge(left, right.cons2))
 
