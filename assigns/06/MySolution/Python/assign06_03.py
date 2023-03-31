5b294906090cf8ee1a5259e6b5852e476b3119c4
karlav24 <90121031+karlav24@users.noreply.github.com>
2023-03-31 00:23:26 -0400

    3 done

--- assigns/06/MySolution/Python/assign06_03.py
+++ assigns/06/MySolution/Python/assign06_03.py
@@ -10,27 +10,45 @@ HX-2023-03-24: 20 points
 Solving the N-queen puzzle
 """
 ####################################################
+
+def initial(N):
+    lst = []
+    for i in range(N):
+        lst.append(0)
+    return [lst]
+
+def checking(x1, intz, num):
+    check = True
+    if intz == 0:
+        return check
+    for i in range(intz):
+        if (x1[i] == num or (abs(intz - i) == abs(x1[i] - num))):
+            check = False
+            break
+    return check
+
+def get_children(x1, N):
+    i = 1
+    x2 = x1[:]
+    x3 = []
+    intz = 0
+    while intz < N:
+        if x1[intz] != 0:
+            intz = intz + 1
+        else:
+            break
+    while i <= N:
+        if checking(x1, intz, i):
+            x2[intz] = i
+            x3.append(x2[:])
+        i = i + 1
+    return x3
+            
+
 def solve_N_queen_puzzle(N):
-"""
-Please revisit assign04-04.sml.
-A board of size N is a tuple of length N.
-######
-For instance, a tuple (0, 0, 0, 0) stands
-for a board of size 4 (that is, a 4x4 board)
-where there are no queen pieces on the board.
-######
-For instance, a tuple (2, 1, 0, 0) stands
-for a board of size 4 (that is, a 4x4 board)
-where there are two queen pieces; the queen piece
-on the 1st row is on the 2nd column; the queen piece
-on the 2nd row is on the 1st column; the last two rows
-contain no queen pieces.
-######
-This function [solve_N_queen_puzzle] should return
-a stream of ALL the boards of size N that contain N
-queen pieces (one on each row and on each column) such
-that no queen piece on the board can catch any other ones
-on the same board.
-"""
-    raise NotImplementedError
+    init = initial(N)
+    store = gtree_dfs(init, lambda x: get_children(x, len(init[0])))
+    store2 = stream_make_filter(store, lambda x1: tuple(x1) if x1[-1] != 0 else None)
+    return store2
+
 ####################################################
